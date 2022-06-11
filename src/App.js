2e98f54b6f8847a6683570b7d7cf499ae3a0e7df
j0sh05 <jalix8@yahoo.com>
2022-06-11 01:03:28 -1000

    Add Task and Tasks Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 
 function App() {
+  const [tasks, setState] = useState([
+    {
+        id: 1,
+        text: 'Do homework',
+        day: 'Tomorrow at 3 pm',
+        reminder: true
+    },
+    {
+        id: 2, 
+        text: 'Sleep',
+        day: 'In like 3 hours',
+        reminder: false
+    },
+    {
+        id: 3, 
+        text: 'Eat at Cafe',
+        day: 'Everyday',
+        reminder: true
+    }
+])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
